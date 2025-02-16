b077c6609d086b624ceccb451149a86ee74f915b
Adil Echajiai <adechaji@e2r6p9.1337.ma>
2025-02-16 22:12:55 +0100

    yarbi mandirch git stash wnrj3 lhadshy

--- srcs/parsing/expantion.c
+++ srcs/parsing/expantion.c
@@ -6,7 +6,7 @@
 /*   By: adechaji <adechaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 00:29:02 by adechaji          #+#    #+#             */
-/*   Updated: 2025/02/16 19:15:50 by adechaji         ###   ########.fr       */
+/*   Updated: 2025/02/16 21:59:42 by adechaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,9 @@ char	*expand_it(char *token)
 	char	*name_var;
 	char	*value_var;
 	char	*home_dir;
+	char	*res;
+	char	*content;
+	char	*expanded;
 	// char	*exit_stat;
 
 	// if (ft_strncmp(token, "$?", 2) == 0)
@@ -38,7 +41,19 @@ char	*expand_it(char *token)
 	// 	exit_stat = ft_itoa wla perror(exit stats);
 	// 	return (exit stat);
 	// }
-	// if 
+	printf("%s\n", token);
+	if (token[0] == '\'' && token[ft_strlen(token) - 1] == '\'')
+	{
+		res = ft_substr(token, 1, ft_strlen(token) - 2);
+		return (res);
+	}
+	if (token[0] == '"' && token[ft_strlen(token) - 1] == '"')
+	{
+		content = ft_substr(token, 1, ft_strlen(token) - 2);
+		expanded = expand_it(content);
+		free(content);
+		return (expanded);
+	}
 	if (token[0] == '$')
 	{
 		name_var = token + 1;
