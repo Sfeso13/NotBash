f35b509c71ad5239ad284e979a7ce7bf1dbec79e
Youssef Hossni <yhossni@e3r2p16.1337.ma>
2025-02-20 13:32:37 +0100

    trying to overwrite the remote branch because of a divergence

---
+++ objs/export.d
@@ -0,0 +1,9 @@
+objs/export.o: srcs/exec/builtins/environment/export.c \
+  srcs/exec/builtins/environment/../../../../includes/exec/exec.h \
+  srcs/exec/builtins/environment/../../../../includes/exec/../global/structs.h \
+  srcs/exec/builtins/environment/../../../../includes/exec/../global/minishell.h \
+  srcs/exec/builtins/environment/../../../../includes/exec/../global/../exec/exec.h \
+  srcs/exec/builtins/environment/../../../../includes/exec/../global/../exec/exec_utils.h \
+  srcs/exec/builtins/environment/../../../../includes/exec/../global/../exec/builtins.h \
+  srcs/exec/builtins/environment/../../../../includes/exec/../global/../parsing/parsing.h \
+  srcs/exec/builtins/environment/../../../../includes/exec/../global/helpers.h
---
+++ objs/improved_cmp.d
@@ -0,0 +1,9 @@
+objs/improved_cmp.o: srcs/exec/improved_cmp.c \
+  srcs/exec/../../includes/exec/exec.h \
+  srcs/exec/../../includes/exec/../global/structs.h \
+  srcs/exec/../../includes/exec/../global/minishell.h \
+  srcs/exec/../../includes/exec/../global/../exec/exec.h \
+  srcs/exec/../../includes/exec/../global/../exec/exec_utils.h \
+  srcs/exec/../../includes/exec/../global/../exec/builtins.h \
+  srcs/exec/../../includes/exec/../global/../parsing/parsing.h \
+  srcs/exec/../../includes/exec/../global/helpers.h
